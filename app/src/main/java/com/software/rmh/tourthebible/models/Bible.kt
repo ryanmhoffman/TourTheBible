30d88df8410a95c1cb3e23fa0b1409c125dd1ce3
Ryan M. Hoffman <rmh.software@gmail.com>
2017-12-22 15:00:55 -0500

    add gson dependency and update Books.kt

--- app/src/main/java/com/software/rmh/tourthebible/models/Bible.kt
+++ app/src/main/java/com/software/rmh/tourthebible/models/Bible.kt
@@ -3,11 +3,15 @@ package com.software.rmh.tourthebible.models
 /**
  * Created by rhoffman on 12/18/17.
  *
- * The class that will interact with all endpoints of the API and feed all of the values
- * from each endpoint. It will be an entire "Bible" so to speak.
+ * The class that interacts with all endpoints of the API and feeds all of the values
+ * from each endpoint. It is an entire "Bible" so to speak.
  */
 class Bible {
     // Endpoint the returns all of the books in the KJV Bible along with their number in order.
     val booksEndpoint = "https://getbible.net/index.php?option=com_getbible&task=bible.books&format=json&v=kjv"
+
+    fun getBookList(): Array<String> {
+        return arrayOf("")
+    }
 
 }
--- app/src/main/java/com/software/rmh/tourthebible/models/Books.kt
+++ app/src/main/java/com/software/rmh/tourthebible/models/Books.kt
@@ -1,10 +1,12 @@
 package com.software.rmh.tourthebible.models
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by rhoffman on 12/18/17.
  */
 data class Books(
-        val book_name: String,
-        val book_nr: Int,
-        val ref: String
+        @SerializedName("book_name") val bookName: String,
+        @SerializedName("book_nr") val bookNumber: Int,
+        @SerializedName("ref") val bookRefName: String
 )
