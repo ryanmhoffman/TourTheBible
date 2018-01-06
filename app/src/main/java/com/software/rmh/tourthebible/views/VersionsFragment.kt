139a391bccd18554082fc43f1030c6b6848fe48d
Ryan M. Hoffman <rmh.software@gmail.com>
2018-01-06 16:16:35 -0500

    add onclick listener to version list

--- app/src/main/java/com/software/rmh/tourthebible/views/VersionsFragment.kt
+++ app/src/main/java/com/software/rmh/tourthebible/views/VersionsFragment.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 import com.software.rmh.tourthebible.R
 
 /**
@@ -17,17 +19,23 @@ import com.software.rmh.tourthebible.R
 class VersionsFragment : Fragment() {
 
     private var versionList: ListView? = null
+    private var versions: Array<String>? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater!!.inflate(R.layout.fragment_versions, container, false)
 
+        versions = view.context.resources.getStringArray(R.array.versions)
+
         if(view is ListView){
             versionList = view
             val adapter = ArrayAdapter(view.context, android.R.layout.simple_list_item_1,
-                    view.context.resources.getStringArray(R.array.versions))
+                    versions)
             versionList!!.adapter = adapter
+
+            versionList!!.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+                Toast.makeText(parent.context, "Clicked " + versions!![position], Toast.LENGTH_SHORT).show() }
         }
 
         return view
